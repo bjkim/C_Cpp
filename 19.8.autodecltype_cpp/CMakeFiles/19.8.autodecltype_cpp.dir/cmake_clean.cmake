faa528b0e722cc04398bc0f700750a47d3c94815
kim8052516@hotmail.com <kim8052516@hotmail.com>
2021-12-01 10:18:16 +0900

    first commit

---
+++ 19.8.autodecltype_cpp/CMakeFiles/19.8.autodecltype_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "19.8.autodecltype_cpp.exe"
+  "19.8.autodecltype_cpp.exe.manifest"
+  "19.8.autodecltype_cpp.pdb"
+  "CMakeFiles/19.8.autodecltype_cpp.dir/main.cpp.obj"
+  "CMakeFiles/19.8.autodecltype_cpp.dir/main.cpp.obj.d"
+  "lib19.8.autodecltype_cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/19.8.autodecltype_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 19.8.autodecltype_cpp/CMakeFiles/19.8.autodecltype_cpp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 19.8.autodecltype_cpp.
+# This may be replaced when dependencies are built.
